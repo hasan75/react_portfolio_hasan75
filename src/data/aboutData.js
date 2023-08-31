5bdaefdc3a7c7b665aab10915d5ddb97d27af282
hasan75 <hasanjab14@gmail.com>
2023-08-31 09:12:32 +0600

    about me section changed

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "Hello, It's Hasan Ahmed. I'm a competitive programmer and web developer based in Bangladesh.",
-    description2: "I have been working with modern web developments materials and technologies like JS, HTML5, CSS3, Bootstrap V5, Tailwind, Material Ui, React, etc. As I have been practicing object-oriented programming, I love to write JS codes. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas. Besides all of this, I always love to explore the countryside.",
-    image: 2
-}
+  title: 'Who I am',
+  description1:
+    "Hello, It's Hasan Ahmed. I'm a competitive programmer and a passionate software engineer based in Bangladesh.",
+  description2:
+    "I specialize in modern web development technologies, including JavaScript, PHP and have experience with frameworks like React, NextJs, VueJs and NuxtJs. My proficiency extends to frontend libraries like Bootstrap V5, Tailwind CSS, Prime Vue and Material UI, enabling me to craft engaging user interface. Beyond the web, I delve into the world of object-oriented programming, finding joy in crafting elegant solutions. Lately, I've been exploring the exciting realms of machine learning and AI, expanding my horizons in this ever-evolving field. My commitment to learning fuels my drive. I relish the opportunity to embrace new challenges and concepts. When I'm not immersed in code, you'll find me exploring the serene countryside, finding inspiration in nature.",
+  image: 2,
+};
